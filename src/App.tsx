e5db787b32df85e9be28b9b86f3c9c18a73f80f8
Matvey <matviyvac@gmail.com>
2025-03-03 19:26:08 +0200

    fix: removed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 
 function App() {
 	return (
 		<div className='container'>
-			<Routes>
-				<Route path='/' element={<Home />} />
-			</Routes>
+			<Home />
 		</div>
 	)
 }
